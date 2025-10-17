15b5fffa78653026e9eb72c1ae7510cdcc40a929
Murali-v123 <muralivmuraliv89@gmail.com>
2025-10-17 15:41:38 +0530

    React

---
+++ React/Day8/React_router/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import wall from "../assets/wall.png";
+
+function Home() {
+  return (
+    <>
+      <h1>🏠 😎Welcome to the Home Page</h1>
+      <img src={wall} alt="wall" width={500}/>
+      
+    </>
+  );
+}
+
+export default Home;
